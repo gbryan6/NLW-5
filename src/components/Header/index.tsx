f8b19eea0961b4b8ce8e9d07652acf36aac60c7d
Gabriel Bryan <gbryan6@hotmail.com>
2021-04-22 00:38:52 -0300

    Faz header e player da pagina

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import styles from './styles.module.scss'
+import format from 'date-fns/format'
+import ptBR from 'date-fns/locale/pt-BR'
 // import { Container } from './styles';
 
 const Header: React.FC = () => {
+  const  currentDate = format(new Date(), 'EEEEEE, d MMMM', {
+    locale: ptBR,
+  })
+
   return (
     <header className={styles.headerContainer}>
       <img src="/logo.svg" alt="Podcastr"/>
       <p>O melhor para você ouvir sempre</p>
-      <span>Quinta, 8 Abril</span>
+      <span>{currentDate}</span>
     </header>  
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,17 @@
-import Head from 'next/head'
-import Header from '../components/Header'
-
-
-export default function Home() {
+export default function Home(props) {
   return (
-    <Header />
+    <h1>index</h1>
   )
+}
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data
+    },
+    revalidate : 60 * 60 * 8,
+  }
 }
