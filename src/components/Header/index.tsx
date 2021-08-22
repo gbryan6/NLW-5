514de86178e4b605c867df4c78e26603d29ec7a5
Gabriel Bryan <gbryan6@hotmail.com>
2021-08-22 15:23:49 -0300

    Faz estilização da pagina de episodio

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,21 +1,25 @@
-import React from 'react';
-import styles from './styles.module.scss'
-import format from 'date-fns/format'
-import ptBR from 'date-fns/locale/pt-BR'
+import React from "react";
+import Link from "next/link";
+import styles from "./styles.module.scss";
+import format from "date-fns/format";
+import ptBR from "date-fns/locale/pt-BR";
 // import { Container } from './styles';
 
 const Header: React.FC = () => {
-  const  currentDate = format(new Date(), 'EEEEEE, d MMMM', {
+  const currentDate = format(new Date(), "EEEEEE, d MMMM", {
     locale: ptBR,
-  })
+  });
 
   return (
     <header className={styles.headerContainer}>
-      <img src="/logo.svg" alt="Podcastr"/>
+      <Link href="/">
+        <img src="/logo.svg" alt="Podcastr" />
+      </Link>
+
       <p>O melhor para você ouvir sempre</p>
       <span>{currentDate}</span>
-    </header>  
+    </header>
   );
-}
+};
 
 export default Header;
